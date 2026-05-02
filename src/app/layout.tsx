730c8dcd2d76cf1ebd8cefe8c8313951a3a8f029
saurav_7709 <shailendrakumarsingh13316@gmail.com>
2026-05-02 12:10:03 +0530

    02: AUth Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,11 @@ import { IBM_Plex_Mono, Inter } from "next/font/google";
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { ThemeProvider } from "@/components/theme-provider";
 
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import {dark} from "@clerk/themes"
+
 import "./globals.css";
+ 
 
 const inter = Inter({
   variable: "--font-inter",
@@ -28,7 +32,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider appearance={dark}>
+      <html lang="en" suppressHydrationWarning>
       <body
          className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
@@ -38,10 +43,27 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <TooltipProvider>{children}</TooltipProvider>
+          
+          <TooltipProvider>
+            <header>
+                <Show when="signed-out">
+            
+                  <SignInButton />
+                  <SignUpButton>
+                    <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                      Sign Up
+                    </button>
+                  </SignUpButton>
+                </Show>
+                <Show when="signed-in">
+                  <UserButton />
+                </Show>
+            </header>
+            {children}</TooltipProvider>
         </ThemeProvider>
         
       </body>
     </html>
+    </ClerkProvider>
   );
 }
